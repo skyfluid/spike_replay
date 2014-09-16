22b1548b7ec13dcf1c2a6928085c0c725789c372
Terry Chia-Hung Liu <terryliu.phd@gmail.com>
2014-09-15 23:59:10 -0700

    rename MCSVM -> 2WaySVM

--- Replay_ident_2WaySVM.m
+++ Replay_ident_2WaySVM.m
@@ -4,8 +4,8 @@ close all
 %wordtoVerify = {'CLAW'}% 'CRICKET' 'FLAG' 'FORK' 'LION' 'MEDAL' 'OYSTER' 'SERPENT' 'SHELF' 'SHIRT'};
 wordtoVerify = {'CLAW' 'CRICKET' 'FLAG' 'FORK' 'LION' 'MEDAL' 'OYSTER' 'SERPENT' 'SHELF' 'SHIRT'};
 
-unit_sel = [5, 6, 7, 8, 9, 23, 42, 43, 55, 57, 60, 61, 62, 66, 67, 71];
-%unit_sel = [1, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 23, 24, 30, 33, 39, 40, 41, 42, 44, 45, 46, 47, 49, 50, 51, 53, 57, 60, 65, 66, 67, 68, 69, 70, 73, 74, 75, 76, 77, 78, 80, 82, 83, 84, 86, 87, 88, 89, 93, 94, 95, 97, 102, 103, 104, 105, 111, 112, 113, 114, 118, 120, 121, 129, 130, 131, 132, 134, 135, 136, 138, 141, 144, 145, 151, 154, 155, 157, 158, 159, 166, 167, 168, 169, 170, 171, 174, 181, 189, 193];
+%unit_sel = [5, 6, 7, 8, 9, 23, 42, 43, 55, 57, 60, 61, 62, 66, 67, 71];
+unit_sel = [1, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 23, 24, 30, 33, 39, 40, 41, 42, 44, 45, 46, 47, 49, 50, 51, 53, 57, 60, 65, 66, 67, 68, 69, 70, 73, 74, 75, 76, 77, 78, 80, 82, 83, 84, 86, 87, 88, 89, 93, 94, 95, 97, 102, 103, 104, 105, 111, 112, 113, 114, 118, 120, 121, 129, 130, 131, 132, 134, 135, 136, 138, 141, 144, 145, 151, 154, 155, 157, 158, 159, 166, 167, 168, 169, 170, 171, 174, 181, 189, 193];
 %unit_sel = [1:199];
 bin_sel = [1:19];
 
@@ -14,9 +14,14 @@ def_binsize = 50;
 def_bin_count = length(bin_sel);
 def_cell_count = length(unit_sel);
 
-load(['./AvgSpikePerSec_' def_epoch_name '_bin' int2str(def_binsize) '.mat']);                               % load firing bin map of the epoch
-   load(['../2WaySVM/stat_total_balance_' int2str(def_cell_count) '_' int2str(def_binsize) 'ms.mat']);     % load SVM files
+def_ylim = [0 0.1; 0 0.1; 0.5 0.8; 0.2 0.4; 0.6 0.8; 0.5 0.7; 0 0.1; 0.7 0.9; 0.4 0.8; 0.2 0.5];
 
+load(['./data/AvgSpikePerSec_' def_epoch_name '_bin' int2str(def_binsize) '.mat']);                               % load firing bin map of the epoch
+load(['../2WaySVM/stat_total_balance_' int2str(def_cell_count) '_' int2str(def_binsize) 'ms.mat']);     % load SVM files
+load('../SA_Units/mg49_sa_event.mat')
+
+mkdir('./data');
+mkdir('./plots');
 
 %{
 %%%%%%%%%%%%%%%%%%%%%%%%% 16 / pre
@@ -31,7 +36,7 @@ def_binsize = 50;
 def_bin_count = length(bin_sel);
 def_cell_count = length(unit_sel);
 
-load(['./AvgSpikePerSec_' def_epoch_name '_bin' int2str(def_binsize) '.mat']);                               % load firing bin map of the epoch
+load(['./data/AvgSpikePerSec_' def_epoch_name '_bin' int2str(def_binsize) '.mat']);                               % load firing bin map of the epoch
 %load(['../2WaySVM/stat_total_balance_' int2str(def_cell_count) '_' int2str(def_binsize) 'ms.mat']);     % load SVM files
 
 OBJ     = PRE_bin;
@@ -51,7 +56,7 @@ for iTime=1:(length(OBJ_sel(1,:))-(def_bin_count-1))
     stampSet = [stampSet; A(:)'];
 end
 
-save(['./' def_epoch_name '_winArr_' int2str(def_cell_count) 'u_' int2str(def_binsize) 'bs.mat'], 'stampSet', '-v7.3');
+save(['./data/' def_epoch_name '_winArr_' int2str(def_cell_count) 'u_' int2str(def_binsize) 'bs.mat'], 'stampSet', '-v7.3');
 
 %%%%%%%%%%%%%%%%%%%%%%%%% 16 / post
 
@@ -65,7 +70,7 @@ def_binsize = 50;
 def_bin_count = length(bin_sel);
 def_cell_count = length(unit_sel);
 
-load(['./AvgSpikePerSec_' def_epoch_name '_bin' int2str(def_binsize) '.mat']);                               % load firing bin map of the epoch
+load(['./data/AvgSpikePerSec_' def_epoch_name '_bin' int2str(def_binsize) '.mat']);                               % load firing bin map of the epoch
 load(['../2WaySVM/stat_total_balance_' int2str(def_cell_count) '_' int2str(def_binsize) 'ms.mat']);     % load SVM files
 
 OBJ     = POST_bin;
@@ -85,7 +90,7 @@ for iTime=1:(length(OBJ_sel(1,:))-(def_bin_count-1))
     stampSet = [stampSet; A(:)'];
 end
 
-save(['./' def_epoch_name '_winArr_' int2str(def_cell_count) 'u_' int2str(def_binsize) 'bs.mat'], 'stampSet', '-v7.3');
+save(['./data/' def_epoch_name '_winArr_' int2str(def_cell_count) 'u_' int2str(def_binsize) 'bs.mat'], 'stampSet', '-v7.3');
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%% 91 / pre
@@ -100,7 +105,7 @@ def_binsize = 50;
 def_bin_count = length(bin_sel);
 def_cell_count = length(unit_sel);
 
-load(['./AvgSpikePerSec_' def_epoch_name '_bin' int2str(def_binsize) '.mat']);                               % load firing bin map of the epoch
+load(['./data/AvgSpikePerSec_' def_epoch_name '_bin' int2str(def_binsize) '.mat']);                               % load firing bin map of the epoch
 load(['../2WaySVM/stat_total_balance_' int2str(def_cell_count) '_' int2str(def_binsize) 'ms.mat']);     % load SVM files
 
 OBJ     = PRE_bin;
@@ -120,7 +125,7 @@ for iTime=1:(length(OBJ_sel(1,:))-(def_bin_count-1))
     stampSet = [stampSet; A(:)'];
 end
 
-save(['./' def_epoch_name '_winArr_' int2str(def_cell_count) 'u_' int2str(def_binsize) 'bs.mat'], 'stampSet', '-v7.3');
+save(['./data/' def_epoch_name '_winArr_' int2str(def_cell_count) 'u_' int2str(def_binsize) 'bs.mat'], 'stampSet', '-v7.3');
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%% 91 / post
@@ -135,7 +140,7 @@ def_binsize = 50;
 def_bin_count = length(bin_sel);
 def_cell_count = length(unit_sel);
 
-load(['./AvgSpikePerSec_' def_epoch_name '_bin' int2str(def_binsize) '.mat']);                               % load firing bin map of the epoch
+load(['../data/AvgSpikePerSec_' def_epoch_name '_bin' int2str(def_binsize) '.mat']);                               % load firing bin map of the epoch
 load(['../2WaySVM/stat_total_balance_' int2str(def_cell_count) '_' int2str(def_binsize) 'ms.mat']);     % load SVM files
 
 OBJ     = POST_bin;
@@ -155,7 +160,7 @@ for iTime=1:(length(OBJ_sel(1,:))-(def_bin_count-1))
     stampSet = [stampSet; A(:)'];
 end
 
-save(['./' def_epoch_name '_winArr_' int2str(def_cell_count) 'u_' int2str(def_binsize) 'bs.mat'], 'stampSet', '-v7.3');
+save(['./data/' def_epoch_name '_winArr_' int2str(def_cell_count) 'u_' int2str(def_binsize) 'bs.mat'], 'stampSet', '-v7.3');
 
 %%%%%%%%%%%%%%%%%%%%%%%%% 199 / pre
 
@@ -169,7 +174,7 @@ def_binsize = 50;
 def_bin_count = length(bin_sel);
 def_cell_count = length(unit_sel);
 
-load(['./AvgSpikePerSec_' def_epoch_name '_bin' int2str(def_binsize) '.mat']);                               % load firing bin map of the epoch
+load(['./data/AvgSpikePerSec_' def_epoch_name '_bin' int2str(def_binsize) '.mat']);                               % load firing bin map of the epoch
 load(['../2WaySVM/stat_total_balance_' int2str(def_cell_count) '_' int2str(def_binsize) 'ms.mat']);     % load SVM files
 
 OBJ     = PRE_bin;
@@ -189,7 +194,7 @@ for iTime=1:(length(OBJ_sel(1,:))-(def_bin_count-1))
     stampSet = [stampSet; A(:)'];
 end
 
-save(['./' def_epoch_name '_winArr_' int2str(def_cell_count) 'u_' int2str(def_binsize) 'bs.mat'], 'stampSet', '-v7.3');
+save(['./data/' def_epoch_name '_winArr_' int2str(def_cell_count) 'u_' int2str(def_binsize) 'bs.mat'], 'stampSet', '-v7.3');
 
 %%%%%%%%%%%%%%%%%%%%%%%%% 199 / task
 
@@ -203,7 +208,7 @@ def_binsize = 50;
 def_bin_count = length(bin_sel);
 def_cell_count = length(unit_sel);
 
-load(['./AvgSpikePerSec_' def_epoch_name '_bin' int2str(def_binsize) '.mat']);                               % load firing bin map of the epoch
+load(['./data/AvgSpikePerSec_' def_epoch_name '_bin' int2str(def_binsize) '.mat']);                               % load firing bin map of the epoch
 load(['../2WaySVM/stat_total_balance_' int2str(def_cell_count) '_' int2str(def_binsize) 'ms.mat']);     % load SVM files
 
 OBJ     = TASK_bin;
@@ -223,7 +228,7 @@ for iTime=1:(length(OBJ_sel(1,:))-(def_bin_count-1))
     stampSet = [stampSet; A(:)'];
 end
 
-save(['./' def_epoch_name '_winArr_' int2str(def_cell_count) 'u_' int2str(def_binsize) 'bs.mat'], 'stampSet', '-v7.3');
+save(['./data/' def_epoch_name '_winArr_' int2str(def_cell_count) 'u_' int2str(def_binsize) 'bs.mat'], 'stampSet', '-v7.3');
 
 %%%%%%%%%%%%%%%%%%%%%%%%% 199 / post
 
@@ -237,7 +242,7 @@ def_binsize = 50;
 def_bin_count = length(bin_sel);
 def_cell_count = length(unit_sel);
 
-load(['./AvgSpikePerSec_' def_epoch_name '_bin' int2str(def_binsize) '.mat']);                               % load firing bin map of the epoch
+load(['./data/AvgSpikePerSec_' def_epoch_name '_bin' int2str(def_binsize) '.mat']);                               % load firing bin map of the epoch
 load(['../2WaySVM/stat_total_balance_' int2str(def_cell_count) '_' int2str(def_binsize) 'ms.mat']);     % load SVM files
 
 OBJ     = POST_bin;
@@ -257,11 +262,11 @@ for iTime=1:(length(OBJ_sel(1,:))-(def_bin_count-1))
     stampSet = [stampSet; A(:)'];
 end
 
-save(['./' def_epoch_name '_winArr_' int2str(def_cell_count) 'u_' int2str(def_binsize) 'bs.mat'], 'stampSet', '-v7.3');
+save(['./data/' def_epoch_name '_winArr_' int2str(def_cell_count) 'u_' int2str(def_binsize) 'bs.mat'], 'stampSet', '-v7.3');
 %}
 %%%%%%%%%%%%%%%% stop!!!
 
-load(['./PRE_winArr_' int2str(def_cell_count) 'u_' int2str(def_binsize) 'bs.mat']);
+load(['./data/PRE_winArr_' int2str(def_cell_count) 'u_' int2str(def_binsize) 'bs.mat']);
 
 for iWord=1:length(wordtoVerify)
     
@@ -294,16 +299,16 @@ for iWord=1:length(wordtoVerify)
     for iTime=1:length(stampSet(:, 1))
         accusum(iTime) = B(iTime)/iTime;
     end
-    %save(['./score_plot_balance_' def_epoch_name '_' int2str(def_cell_count) 'u_' int2str(def_binsize) 'ms' ], 'score_plot', '-v7.3')
+    %save(['./data/score_plot_balance_' def_epoch_name '_' int2str(def_cell_count) 'u_' int2str(def_binsize) 'ms' ], 'score_plot', '-v7.3')
 
     subplot(1, 3, 1), plot(accusum);
     %plot(cumsum(score_plot{iWord}))
-    xlabel('Bin #'); ylabel('Cumulative sum');
-    ylim([0 1]);
+    xlabel('Bin #'); ylabel('Average # of Hits');
+    ylim(def_ylim(iWord, :));
     title(['PRE ' int2str(def_cell_count) 'u ' int2str(def_binsize) 'ms']);
 
     %%% TASK
-    load(['./TASK_winArr_' int2str(def_cell_count) 'u_' int2str(def_binsize) 'bs.mat']);
+    load(['./data/TASK_winArr_' int2str(def_cell_count) 'u_' int2str(def_binsize) 'bs.mat']);
    
     % find the best SVM
     best_SVM = [];
@@ -331,17 +336,27 @@ for iWord=1:length(wordtoVerify)
     for iTime=1:length(stampSet(:, 1))
         accusum(iTime) = B(iTime)/iTime;
     end
-    %save(['./score_plot_balance_' def_epoch_name '_' int2str(def_cell_count) 'u_' int2str(def_binsize) 'ms' ], 'score_plot', '-v7.3')
+    %save(['./data/score_plot_balance_' def_epoch_name '_' int2str(def_cell_count) 'u_' int2str(def_binsize) 'ms' ], 'score_plot', '-v7.3')
 
     subplot(1, 3, 2), plot(accusum);
     %plot(cumsum(score_plot{iWord}))
-    xlabel('Bin #'); ylabel('Cumulative sum');
-    ylim([0 1]);
+    xlabel('Bin #'); ylabel('Average # of Hits');
+    ylim(def_ylim(iWord, :));
     title(['TASK ' int2str(def_cell_count) 'u ' int2str(def_binsize) 'ms']);
 
+    hold on
+    % add dots
+    for iEvent=1:length(event)
+        if strcmp(event(iEvent).type, wordtoVerify{iWord})
+            t = event(iEvent).timestamp - 78974160)/(30*def_binsize);
+            plot(t, accusum(floor(t)), 'or');
+            hold on
+        end
+    end
+    
     %%%POST
     
-    load(['./POST_winArr_' int2str(def_cell_count) 'u_' int2str(def_binsize) 'bs.mat']);
+    load(['./data/POST_winArr_' int2str(def_cell_count) 'u_' int2str(def_binsize) 'bs.mat']);
     
     % find the best SVM
     best_SVM = [];
@@ -369,14 +384,14 @@ for iWord=1:length(wordtoVerify)
     for iTime=1:length(stampSet(:, 1))
         accusum(iTime) = B(iTime)/iTime;
     end
-    %save(['./score_plot_balance_' def_epoch_name '_' int2str(def_cell_count) 'u_' int2str(def_binsize) 'ms' ], 'score_plot', '-v7.3')
+    %save(['./data/score_plot_balance_' def_epoch_name '_' int2str(def_cell_count) 'u_' int2str(def_binsize) 'ms' ], 'score_plot', '-v7.3')
 
     subplot(1, 3, 3), plot(accusum);
     %plot(cumsum(score_plot{iWord}))
-    xlabel('Bin #'); ylabel('Cumulative sum');
-    ylim([0 1]);
+    xlabel('Bin #'); ylabel('Average # of Hits');
+    ylim(def_ylim(iWord, :));
     title(['POST ' int2str(def_cell_count) 'u ' int2str(def_binsize) 'ms']);
 
-    saveas(gcf, ['./' wordtoVerify{iWord} '_accu_' int2str(def_binsize) 'ms.jpg'], 'jpg');
+    saveas(gcf, ['./plots/' wordtoVerify{iWord} '_accu_' int2str(def_binsize) 'ms.jpg'], 'jpg');
 end
 %}
